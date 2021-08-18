1a688e538d68be8723ada2aa0a80efd4f848349d
syed shaida <syedshaida295@gmail.com>
2021-08-18 15:40:32 +0530

    processing and output done

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const errorMessage = document.querySelector("#error-message")
+const noOfNotes = document.querySelectorAll(".no-of-notes")
+
+const availableNotes = [2000,500,100,20,10,5,1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
@@ -16,13 +19,19 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
     } else {
         showMessage("Bill amount should be greater than 0");
     }
-});
+})
 
-function calculateChange(){
+function calculateChange(amountToBeReturned) {
+    for(let i = 0; i < availableNotes.length; i++){
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        amountToBeReturned %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
 
+
+    }
 }
 
-function hideMessage(){
+function hideMessage() {
     errorMessage.style.display = "none"
 }
 
